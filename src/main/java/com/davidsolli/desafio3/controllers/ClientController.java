4effb59ca5e5224a8b165795fe5bf4012465949b
Davidsolli <davidolisoa@gmail.com>
2024-08-07 22:24:50 -0300

    busca pagina com GET

--- src/main/java/com/davidsolli/desafio3/controllers/ClientController.java
+++ src/main/java/com/davidsolli/desafio3/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.davidsolli.desafio3.controllers;
 import com.davidsolli.desafio3.DTO.ClientDTO;
 import com.davidsolli.desafio3.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,5 +22,11 @@ public class ClientController {
     public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
         ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> clientPage = clientService.findAll(pageable);
+        return ResponseEntity.ok(clientPage);
     }
 }
